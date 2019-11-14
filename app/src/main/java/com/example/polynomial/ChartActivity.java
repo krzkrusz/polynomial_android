13136801881bf0dbc8d6b32d2ef2dd8b67be0004
kkrus <kkruszynski1993@wp.pl>
2019-11-14 23:46:58 +0100

    save picture implemented

--- app/src/main/java/com/example/polynomial/ChartActivity.java
+++ app/src/main/java/com/example/polynomial/ChartActivity.java
@@ -1,8 +1,13 @@
 package com.example.polynomial;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +15,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.softmoore.android.graphlib.Graph;
 import com.softmoore.android.graphlib.GraphView;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.math3.analysis.solvers.LaguerreSolver;
 import org.apache.commons.math3.complex.Complex;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +29,7 @@ import java.util.stream.Collectors;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ChartActivity extends AppCompatActivity {
 
@@ -28,6 +38,9 @@ public class ChartActivity extends AppCompatActivity {
 
     @BindView(R.id.graph_view)
     GraphView graphView;
+
+    @BindView(R.id.button2)
+    Button button;
 
     private List<Double> roots = new ArrayList<>();
 
@@ -69,6 +82,34 @@ public class ChartActivity extends AppCompatActivity {
         return Arrays.stream(roots)
                 .map(Complex::getReal)
                 .collect(Collectors.toList());
+    }
+
+    @OnClick(R.id.button2)
+    protected void savePicture() {
+        Bitmap bitmap = Bitmap.createBitmap(graphView.getWidth(), graphView.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Drawable bgDrawable = graphView.getBackground();
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas);
+        } else {
+            canvas.drawColor(Color.WHITE);
+        }
+        graphView.draw(canvas);
+
+
+        File file = this.getApplicationContext().getExternalFilesDir(null);
+        file = new File(file, "graph_picture.jpg"); //storage/emulated/0/Android/data/com.example.polynomial/files/graph_picture.jpg
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 95, fos);
+        } catch (FileNotFoundException e) {
+            Log.d("GRAPH", "Error during saving picture");
+        } finally {
+            if (fos != null) {
+                IOUtils.closeQuietly(fos);
+            }
+        }
     }
 
 }
